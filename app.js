045177b8130066733cd3956647fd8e8b26810f05
Karla Gussnecker <karla.gussnecker@gmail.com>
2022-03-01 20:25:46 +0100

    dkjvn

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '/client/build')));
 
 app.get("*", (req, res) => { 
-    res.sendFile(path.join(__dirname + '/client/build/index.html')); 
+    res.sendFile(path.join(__dirname + '/client/build')); 
 });
 
 app.use('/users', usersRouter);
